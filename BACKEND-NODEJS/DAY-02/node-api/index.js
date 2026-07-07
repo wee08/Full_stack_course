c2a09948aaf46f51cb19d76d6e95f32797de8aa9
wee08 <manutnithyasean@gmail.com>
2026-07-07 21:09:34 +0700

    export/import module

--- BACKEND-NODEJS/DAY-02/node-api/index.js
+++ BACKEND-NODEJS/DAY-02/node-api/index.js
@@ -1,7 +1,15 @@
+// const func = require("./src/app.js");
+import { app, sum } from "./src/app.js";
 var person = {
   name: "Dara",
   age: 20,
   address: "Phnom Penh",
 };
 // console.table(person);
-console.time(person);
+// console.time(person);
+
+// func.app();
+// console.log(func.sum(12, 12));
+
+app();
+console.log(sum(2, 12));
---
+++ BACKEND-NODEJS/DAY-02/node-api/src/app.js
@@ -0,0 +1,7 @@
+function app() {
+  console.log("Hello world");
+}
+function sum(a, b) {
+  return a + b;
+}
+module.exports = { app, sum };
