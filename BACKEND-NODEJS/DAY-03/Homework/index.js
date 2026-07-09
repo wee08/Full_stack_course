0c3451f1e4774bea35c412ce8a0cb5691550d6f2
wee08 <manutnithyasean@gmail.com>
2026-07-09 11:04:46 +0700

    init project setup

--- BACKEND-NODEJS/DAY-03/Homework/index.js
+++ BACKEND-NODEJS/DAY-03/Homework/index.js
@@ -1,1 +1,12 @@
-j;
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello api");
+});
+
+app.listen(PORT, () => {
+  console.log(`local host: http://localhost:${PORT}`);
+});
