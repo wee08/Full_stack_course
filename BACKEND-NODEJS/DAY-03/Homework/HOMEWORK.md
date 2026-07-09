7770eee749de8b592b9bd7abe9d44e3c876e3fe1
wee08 <manutnithyasean@gmail.com>
2026-07-09 11:01:08 +0700

    init homework project

---
+++ BACKEND-NODEJS/DAY-03/Homework/HOMEWORK.md
@@ -0,0 +1,19 @@
+homework
+
+1. create route students
+
+- get all students
+- get student by id
+- create student
+- update student
+- delete student
+
+2. create controller students
+
+- get all students
+- get studnet by id
+- create student
+- update student
+- delete student
+
+3. create data student
---
+++ BACKEND-NODEJS/DAY-03/Homework/src/data/students.js
@@ -0,0 +1,38 @@
+const listStudent = [
+  {
+    name: "Sean Manutnithya",
+    id: 1,
+    gender: "M",
+    class: "12 A",
+    "date of birth": "12-Mar-2000",
+  },
+  {
+    name: "Heng Dara",
+    id: 2,
+    gender: "M",
+    class: "12 A",
+    "date of birth": "12-May-2001",
+  },
+  {
+    name: "Meas Somorn",
+    id: 3,
+    gender: "M",
+    class: "12 A",
+    "date of birth": "01-Jun-2008",
+  },
+  {
+    name: "Ly MeyLii",
+    id: 4,
+    gender: "F",
+    class: "12 A",
+    "date of birth": "09-Apr-2006",
+  },
+  {
+    name: "Pov Menghor",
+    id: 5,
+    gender: "M",
+    class: "12 A",
+    "date of birth": "12-Dec-2009",
+  },
+];
+module.exports = listStudent;
