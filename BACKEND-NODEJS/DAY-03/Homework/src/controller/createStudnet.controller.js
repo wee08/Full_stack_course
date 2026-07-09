a61cda83774b98a7540f62823da96cad67ff61fe
wee08 <manutnithyasean@gmail.com>
2026-07-09 21:02:23 +0700

    create new student function

--- BACKEND-NODEJS/DAY-03/Homework/src/controller/createStudnet.controller.js
+++ BACKEND-NODEJS/DAY-03/Homework/src/controller/createStudnet.controller.js
@@ -1,4 +1,19 @@
+const listStudent = require("../data/students");
 function createStudent(req, res) {
-  res.send("create student");
+  const newStudnet = [
+    {
+      name: "Leap heng",
+      id: listStudent.length + 1,
+      gender: "M",
+      class: "12 A",
+      "date of birth": "02-Mar-2000",
+    },
+  ];
+
+  listStudent.push(newStudnet);
+  res.send({
+    student: "added sucessfuly",
+    student: listStudent,
+  });
 }
 module.exports = createStudent;
