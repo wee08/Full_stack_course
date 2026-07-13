5c212ffa7e06b6610edba557e62c96dcb65b1268
wee08 <manutnithyasean@gmail.com>
2026-07-13 10:47:40 +0700

    add proudct to cart as an array

--- BACKEND-NODEJS/DAY-04/src/controller/createProduct.controller.js
+++ BACKEND-NODEJS/DAY-04/src/controller/createProduct.controller.js
@@ -1,13 +1,21 @@
 const productList = require("../data/product");
 
 const createProduct = (req, res) => {
-  const newProduct = {
-    product: "Skate Board",
-    id: productList.length + 1,
-    price: 95000,
-    color: "blue",
-  };
-  productList.push(newProduct);
+  const products =
+    Array.isArray(req.query.product) ? req.query.product : [req.query.product];
+  const prices =
+    Array.isArray(req.query.price) ? req.query.price : [req.query.price];
+  const colors =
+    Array.isArray(req.query.color) ? req.query.color : [req.query.color];
+
+  const newProduct = products.map((name, i) => ({
+    product: name,
+    id: productList.length + i + 1,
+    price: Number(prices[i]),
+    color: colors[i],
+  }));
+
+  productList.push(...newProduct);
   res.send({
     product: productList,
   });
