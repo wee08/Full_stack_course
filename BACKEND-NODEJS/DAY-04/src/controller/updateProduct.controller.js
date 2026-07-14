663147bacd884b0ed6c6be0437d9886e0a043190
wee08 <manutnithyasean@gmail.com>
2026-07-14 19:21:13 +0700

    update product using put for postman

--- BACKEND-NODEJS/DAY-04/src/controller/updateProduct.controller.js
+++ BACKEND-NODEJS/DAY-04/src/controller/updateProduct.controller.js
@@ -1,22 +1,15 @@
 const products = require("../data/product");
 
 const updateProduct = (req, res) => {
-  const id = req.query.id;
-  const products =
-    Array.isArray(req.query.product) ? req.query.product : [req.query.product];
-  const prices =
-    Array.isArray(req.query.price) ? req.query.price : [req.query.price];
-  const colors =
-    Array.isArray(req.query.color) ? req.query.color : [req.query.color];
-
-  const targetProduct = products.map((item, idx) => {
+  const { id, name, price, color } = req.body;
+  const newProduct = {
+    name,
+    id: Number(id),
+    price: Number(price),
+    color,
+  };
+  products.map((item, idx) => {
     let replace_idx;
-    const newProduct = {
-      product: products[idx],
-      id: Number(id[idx]),
-      price: Number(prices[idx]),
-      color: colors[idx],
-    };
     if (item.id == id) {
       replace_idx = idx;
     }
