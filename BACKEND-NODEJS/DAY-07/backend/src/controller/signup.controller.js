d5afd66bedc07e5ec814e9cec20df4691cc5bf59
wee08 <manutnithyasean@gmail.com>
2026-07-17 14:44:04 +0700

    build postman, handle user existend and missmatch password

--- BACKEND-NODEJS/DAY-07/backend/src/controller/signup.controller.js
+++ BACKEND-NODEJS/DAY-07/backend/src/controller/signup.controller.js
@@ -1,5 +1,19 @@
 const userData = require("../data/user_data");
 const signup = (req, res) => {
+  const { email, password, confirmPassword, phone } = req.body;
+  const isUserExisted = userData.find((user) => user.email === email);
+  if (isUserExisted) {
+    res.send({
+      message: "user already exist",
+    });
+    return;
+  }
+  if (password !== confirmPassword) {
+    res.send({
+      message: "password doesn't match!",
+    });
+    return;
+  }
   res.send({
     userData,
   });
--- BACKEND-NODEJS/DAY-07/backend/src/router/user.route.js
+++ BACKEND-NODEJS/DAY-07/backend/src/router/user.route.js
@@ -1,6 +1,6 @@
 const signup = require("../controller/signup.controller");
 
 const userRoute = (app) => {
-  app.get("/v1/api/user/signup", signup);
+  app.post("/v1/api/user/signup", signup);
 };
 module.exports = userRoute;
