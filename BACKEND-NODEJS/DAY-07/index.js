6cd39150e3db8a80574a2822a4262e623be172f1
wee08 <manutnithyasean@gmail.com>
2026-07-17 11:30:52 +0700

    init project

--- BACKEND-NODEJS/DAY-07/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const PORT = 3000;
-const app = express();
-
-app.get("/", "homepage");
-app.listen(PORT, () => {
-  console.log(`http://localhost${PORT}`);
-});
