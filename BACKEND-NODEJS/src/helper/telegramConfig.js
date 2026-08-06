300c7d27fa38e65808e9f0db7b1329a56a9673ec
wee08 <manutnithyasean@gmail.com>
2026-08-06 20:37:52 +0700

    fix config telegram

--- BACKEND-NODEJS/src/helper/telegramConfig.js
+++ BACKEND-NODEJS/src/helper/telegramConfig.js
@@ -1,18 +1,21 @@
 const { TelegramBot } = require("node-telegram-bot-api");
-
+const { missing } = require("../helper/validate");
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 const sendMessageToTelegram = async (req, res) => {
+  //     PERSONAL_TELEGRAM_ID = 8966307639
+  // GROUP_TELEGRAM_ID = -1003953131347
   try {
-    const message = "whats up my love!";
-    const chatID = process.env.GROUP_TELEGRAM_ID;
-    await bot.sendMessage(chatID, message);
+    // const { message } = req.body;
+    // missing(req, res, field);
+    // const message = "whats up my love!";
+    await bot.sendMessage("-5457207493", "hi");
     res.send({
       message: "message sent to telegram bot successfuly",
     });
   } catch (error) {
-    logError(error);
+    console.error(error);
   }
 };
 
